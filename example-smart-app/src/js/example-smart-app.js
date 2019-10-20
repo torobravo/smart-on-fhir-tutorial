60e28df77efe721e97383772ffe88f0eea2b4574
torobravo <torobravo@users.noreply.github.com>
2019-10-20 14:46:21 -0400

    weight

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -38,7 +38,9 @@
           }
 
           var height = byCodes('8302-2');         
-          var weight = byCodes('29463-7');          
+          var weight = byCodes('29463-7');     
+          alert(weight);
+          alert(weight[0]);
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -50,7 +52,8 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          p.weight = getQuantityValueAndUnit(weight[0]);          
+          p.weight = getQuantityValueAndUnit(weight[0]);     
+          alert(p.weight);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
